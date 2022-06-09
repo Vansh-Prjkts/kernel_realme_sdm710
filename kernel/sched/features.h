7d3e3057cba313be9b7ce87d14166e7df1f07a95
lllgts <wuer031@126.com>
2022-06-09 05:53:38 +0000

    sched/features: Tweak for performance
    Signed-off-by: lllgts <wuer031@126.com>
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -109,7 +109,7 @@ SCHED_FEAT(MIN_CAPACITY_CAPPING, true)
  * ON: If the target CPU saves any energy, use that.
  * OFF: Use whichever of target or backup saves most.
  */
-SCHED_FEAT(FBT_STRICT_ORDER, false)
+SCHED_FEAT(FBT_STRICT_ORDER, true)
 /*
  * Enforce schedtune.prefer_idle to take need_idle path.
  * ON: schedtune.prefer_idle is replaced with need_idle
